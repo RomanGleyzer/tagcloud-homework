cacf9734eaf18650a2a5e3092b956b76f202aaf3
Roman Gleizer <romy4.gleyzer@mail.ru>
2025-11-22 18:02:53 +0500

    Применяя tdd написал первый тест и начал реализовывать CircularCloudLayouter
    На первый вызов PutNextRectangle создает прямоугольник по центру

---
+++ cs/TagsCloudVisualization/CircularCloudLayouter.cs
@@ -0,0 +1,29 @@
+﻿using System.Drawing;
+
+namespace TagsCloudVisualization;
+
+public class CircularCloudLayouter(Point center)
+{
+    private readonly List<Rectangle> _createdRectangles = [];
+
+    public Rectangle PutNextRectangle(Size rectangleSize)
+    {
+        if (_createdRectangles.Count == 0)
+        {
+            var startRectangle = CreateRectangle(rectangleSize, center.X, center.Y);
+
+            _createdRectangles.Add(startRectangle);
+            return startRectangle;
+        }
+        
+        return new Rectangle();
+    }
+
+    private static Rectangle CreateRectangle(Size size, int centerX, int centerY)
+    {
+        var rectangleX = centerX - size.Width / 2;
+        var rectangleY = centerY - size.Height / 2;
+
+        return new Rectangle(rectangleX, rectangleY, size.Width, size.Height);
+    }
+}
--- cs/TagsCloudVisualization/CircularCloudLayouterTests.cs
+++ cs/TagsCloudVisualization/CircularCloudLayouterTests.cs
@@ -1,15 +1,38 @@
-﻿namespace TagsCloudVisualization;
+﻿using FluentAssertions;
+using FluentAssertions.Execution;
+using System.Drawing;
+
+namespace TagsCloudVisualization;
 
 public class CircularCloudLayouterTests
 {
+    private const int Width = 20;
+    private const int Height = 10;
+
+    private Point _center;
+    private Size _rectangleSize;
+    private CircularCloudLayouter _layouter;
+
     [SetUp]
     public void Setup()
     {
+        _center = Point.Empty;
+        _layouter = new CircularCloudLayouter(_center);
+        _rectangleSize = new Size(Width, Height);
     }
 
     [Test]
-    public void Test1()
+    public void PutNextRectangle_FirstCall_ShouldBeInCenter()
     {
-        Assert.Pass();
+        var rectangle = _layouter.PutNextRectangle(_rectangleSize);
+
+        using (new AssertionScope())
+        {
+            var expectedLeft = _center.X - _rectangleSize.Width / 2;
+            var expectedTop = _center.Y - _rectangleSize.Height / 2;
+
+            rectangle.Left.Should().Be(expectedLeft);
+            rectangle.Top.Should().Be(expectedTop);
+        }
     }
 }
