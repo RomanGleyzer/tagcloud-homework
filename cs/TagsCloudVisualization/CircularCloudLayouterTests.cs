ea4abedcaed436dc2498ce8e8b088b26e6bd76fb
Roman Gleizer <romy4.gleyzer@mail.ru>
2025-11-22 15:59:27 +0500

    Создал проект

---
+++ cs/TagsCloudVisualization/CircularCloudLayouterTests.cs
@@ -0,0 +1,15 @@
+﻿namespace TagsCloudVisualization;
+
+public class CircularCloudLayouterTests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
