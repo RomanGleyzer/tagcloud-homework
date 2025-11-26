c4ac54c746d821fcf03f680a8d53bee4514cbdb8
Roman Gleizer <romy4.gleyzer@mail.ru>
2025-11-26 19:14:09 +0500

    Начальная реализация визуализатора

---
+++ cs/CloudLayouterVisualizer/CloudVisualizer.cs
@@ -0,0 +1,48 @@
+﻿using System.Drawing;
+
+namespace CloudLayouterVisualizer;
+
+public class CloudVisualizer(List<Rectangle> rectangles, string filePath, double scale, int padding)
+{
+    public void Visualize()
+    {
+        var minX = rectangles.Min(rect => rect.Left);
+        var minY = rectangles.Min(rect => rect.Top);
+        var maxX = rectangles.Max(rect => rect.Right);
+        var maxY = rectangles.Max(rect => rect.Bottom);
+
+        var cloudWidth = maxX - minX;
+        var cloudHeight = maxY - minY;
+
+        var scaleWidth = cloudWidth * scale;
+        var scaleHeight = cloudHeight * scale;
+
+        var imageWidth = (int)Math.Ceiling(scaleWidth + 2 * padding);
+        var imageHeight = (int)Math.Ceiling(scaleHeight + 2 * padding);
+
+        using var bitmap = new Bitmap(imageWidth, imageHeight);
+        using var graphics = Graphics.FromImage(bitmap);
+
+        graphics.Clear(Color.White);
+
+        using var fillBrush = new SolidBrush(Color.Orange);
+        using var textBrush = new SolidBrush(Color.Black);
+        using var font = new Font("Arial", 8);
+        using var pen = new Pen(Color.Black, 1);
+
+        foreach (var rect in rectangles)
+        {
+            var x = (float)((rect.X - minX) * scale + padding);
+            var y = (float)((rect.Y - minY) * scale + padding);
+            var width = (float)(rect.Width * scale);
+            var height = (float)(rect.Height * scale);
+
+            var imageRect = new RectangleF(x, y, width, height);
+
+            graphics.FillRectangle(fillBrush, imageRect);
+            graphics.DrawRectangle(pen, imageRect.X, imageRect.Y, imageRect.Width, imageRect.Height);
+        }
+
+        bitmap.Save(filePath);
+    }
+}
--- cs/CloudLayouterVisualizer/Program.cs
+++ cs/CloudLayouterVisualizer/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using TagsCloudVisualization;
+using System.Drawing;
+using CloudLayouterVisualizer;
+
+var layouter = new CircularCloudLayouter(new Point(0, 0));
+var rectangles = new List<Rectangle>();
+
+for (int i = 0; i < 50; i++)
+{
+    rectangles.Add(layouter.PutNextRectangle(new Size(30, 20)));
+}
+
+var visualizer = new CloudVisualizer(rectangles, "cloud.png", 10, 1);
+visualizer.Visualize();
